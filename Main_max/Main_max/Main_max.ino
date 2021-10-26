ccc55709dec54d4255ad1886158e39f01bcf525b
Maxime <maximealberty@gmail.com>
2021-10-26 15:44:40 +0200

    Dev pressure sensor
    Ancien capteur dead !!
    Le problème venait du capteur. 
    Lecture coeff OK.
    Mais pas de valeur sur Temp et Pressure ADC

--- Main_max/Main_max/Main_max.ino
+++ Main_max/Main_max/Main_max.ino
@@ -13,11 +13,13 @@ float a0, b1, b2, c12;
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.print("Session Start!");
   Wire.begin();
-  Serial.print("Wire Init done!");
+  Serial.begin(9600);
+  Serial.println(">>>> Wire Start!");
+  Serial.println(">>>> Serial Start!");
+
   Read_coeff_pression();
+  Serial.println(">>>> Read_coeff_pression done!");
 }
 
 void loop()
@@ -25,122 +27,157 @@ void loop()
   float Pression = 0;
 
   Pression = Read_pression();
-  Serial.print("Pressions = ");
-  Serial.println(Pression);
-  delay(100);
+  Serial.print(">> Pressions = "); Serial.print(Pression); Serial.println(" kPa");
+  delay(1000);
 }
 
 /*******************************************************
- * Fonction de lecture et de calcul de la pression
- */
+   Fonction de lecture et de calcul de la pression
+*/
 float Read_pression()
 {
   uint16_t Temperature_ADC = 0;
   uint16_t Pressure_ADC = 0;
-  float Pressure;
+  float Pcomp, Pressure;
 
   Start_measure_pressure();
 
   /**
-   * Lecture Pression
-   */
-
+     Lecture Pression
+  */
   Wire.beginTransmission(I2C_ADDR);
   Wire.write(I2C_READ_PRESSURE_MSB);
   Wire.endTransmission();
-  
-  Wire.requestFrom(I2C_ADDR, 1);
+
+  Wire.requestFrom(I2C_ADDR, 2);
   if (Wire.available()) {
-    Pressure_ADC = Wire.read();
-    Pressure_ADC = Pressure_ADC << 8;
+    Pressure_ADC = (((uint16_t)Wire.read() << 8) | Wire.read());
   }
+  /*
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(I2C_READ_PRESSURE_MSB);
+    Wire.endTransmission();
 
-  Wire.beginTransmission(I2C_ADDR);
-  Wire.write(I2C_READ_PRESSURE_LSB);
-  Wire.endTransmission();
-  
-  Wire.requestFrom(I2C_ADDR, 1);
-  if (Wire.available())
-    Pressure_ADC |= Wire.read();
+    Wire.requestFrom(I2C_ADDR, 1);
+    if (Wire.available()) {
+      Pressure_ADC = Wire.read();
+      Pressure_ADC = Pressure_ADC << 8;
+    }
 
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(I2C_READ_PRESSURE_LSB);
+    Wire.endTransmission();
+
+    Wire.requestFrom(I2C_ADDR, 1);
+    if (Wire.available())
+      Pressure_ADC |= Wire.read();
+  */
   /**
-   * Lecture Temperature
-   */
+     Lecture Temperature
+  */
 
   Wire.beginTransmission(I2C_ADDR);
   Wire.write(I2C_READ_TEMP_MSB);
   Wire.endTransmission();
-  
-  Wire.requestFrom(I2C_ADDR, 1);
-  if (Wire.available()) {
-    Temperature_ADC = Wire.read();
-    Temperature_ADC = Temperature_ADC << 8;
-  }
 
-  Wire.beginTransmission(I2C_ADDR);
-  Wire.write(I2C_READ_TEMP_LSB);
-  Wire.endTransmission();
-  
-  Wire.requestFrom(I2C_ADDR, 1);
-  if (Wire.available())
-    Temperature_ADC |= Wire.read();
-    
+  Wire.requestFrom(I2C_ADDR, 2);
+  if (Wire.available()) {
+    Temperature_ADC = (((uint16_t)Wire.read() << 8) | Wire.read());
+  }
+  /*
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(I2C_READ_TEMP_MSB);
+    Wire.endTransmission();
+
+    Wire.requestFrom(I2C_ADDR, 1);
+    if (Wire.available()) {
+      Temperature_ADC = Wire.read();
+      Temperature_ADC = Temperature_ADC << 8;
+    }
+
+    Wire.beginTransmission(I2C_ADDR);
+    Wire.write(I2C_READ_TEMP_LSB);
+    Wire.endTransmission();
+
+    Wire.requestFrom(I2C_ADDR, 1);
+    if (Wire.available())
+      Temperature_ADC |= Wire.read();
+  */
+  Serial.println(" ADC Values : ");
+  Serial.print("> Temp = "); Serial.println(Temperature_ADC);
+  Serial.print("> Pressure = "); Serial.println(Pressure_ADC);
+
   /**
-   * Calcul de la pression compenser
-   */
-   Pressure = a0 + (b1 + c12 * Temperature_ADC) * Pressure_ADC + b2 * Temperature_ADC;
-   return Pressure;
+     Calcul de la pression compenser
+  */
+  Pcomp = a0 + (b1 + c12 * Temperature_ADC) * Pressure_ADC + b2 * Temperature_ADC;
+  Pressure = (65 / 1023) * Pcomp + 50;
+  return Pressure;
 }
 
 /**********************************************
- * Fonction de lecture des coefficents pour le capteur de pression
- */
+   Fonction de lecture des coefficents pour le capteur de pression
+*/
 void Read_coeff_pression()
 {
   uint16_t _a0, _b1, _b2, _c12;
   /**
-   * Lecture coefficient
-   */
+     Lecture coefficient
+  */
   Wire.beginTransmission(I2C_ADDR);
   Wire.write(I2C_READ_COEFF);
   Wire.endTransmission();
-  
+
   Wire.requestFrom(I2C_ADDR, 8);
   if (Wire.available()) {
-    _a0 = (Wire.read() << 8) | Wire.read();
-    _b1 = (Wire.read() << 8) | Wire.read();
-    _b2 = (Wire.read() << 8) | Wire.read();
-    _c12 = (Wire.read() << 8) | Wire.read();
+    _a0 = (((uint16_t)Wire.read() << 8) | Wire.read());
+    _b1 = (((uint16_t)Wire.read() << 8) | Wire.read());
+    _b2 = (((uint16_t)Wire.read() << 8) | Wire.read());
+    _c12 = (((uint16_t)Wire.read() << 8) | Wire.read()) >> 2;
   }
 
-  Serial.print("///////////////////////////////");
-  Serial.print("Coefficient brute : ");
+  Serial.println("///////////////////////////////");
+  Serial.println("Coefficient brute : ");
   Serial.print("_a0 = "); Serial.println(_a0);
   Serial.print("_b1 = "); Serial.println(_b1);
   Serial.print("_b2 = "); Serial.println(_b2);
   Serial.print("_c12 = "); Serial.println(_c12);
   Serial.print("///////////////////////////////");
-
-  a0 = (float)_a0 / 8;
-  b1 = (float)_b1 / 8192;
-  b2 = (float)_b2 / 16384;
-  c12 = (float)_c12;
+  
+  a0 = CheckSigned((float)_a0);
+  b1 = CheckSigned((float)_b1);
+  b2 = CheckSigned((float)_b2);
+  c12 = CheckSigned((float)_c12);
+  
+  a0 = a0 / 8;
+  b1 = b1 / 8192;
+  b2 = b2 / 16384;
+  c12 = c12;
   c12 /= 4194304.0;
 
-  Serial.print("Coefficient reel : ");
-  Serial.print("a0 = "); Serial.println(a0);
-  Serial.print("b1 = "); Serial.println(b1);
-  Serial.print("b2 = "); Serial.println(b2);
-  Serial.print("c12 = "); Serial.println(c12);
+  Serial.println("Coefficient reel : ");
+  Serial.print("a0 = "); Serial.println(a0,DEC);
+  Serial.print("b1 = "); Serial.println(b1,DEC);
+  Serial.print("b2 = "); Serial.println(b2,DEC);
+  Serial.print("c12 = "); Serial.println(c12,DEC);
   Serial.print("///////////////////////////////");
+
+}
+
+float CheckSigned(float value)
+{
+  if(value > 32767){ //On test si le bit de signe est à 1
+    value = value - 65536; //Si oui, Complement à 2.
+  }
+  return value;
 }
 
 /************************************************
- * Envoi de la commande I2C pour le démarage d'une lecture de pression et temperature
- */
+   Envoi de la commande I2C pour le démarage d'une lecture de pression et temperature
+*/
 void Start_measure_pressure()
 {
-  Serial.print("Start Measure pressure");
+  Serial.println("// Start Measure pressure \\");
   Wire.beginTransmission(I2C_ADDR);
   Wire.write(I2C_START_CONVERT);
   Wire.endTransmission();
