ac673c6ccd83ac6fac12c492f907b0bf55d0d8b2
Lino Delpech <delpechlino@gmail.com>
2021-10-26 15:46:41 +0200

    Fixed humidity and temp sensor

--- Main_lino/Main_lino.ino
+++ Main_lino/Main_lino.ino
@@ -2,13 +2,10 @@
 #define pin_humTemp_in 5
 
 #define DHT_SUCCESS 0
-#define DHT_TIMEOUT_ERROR 1
-#define DHT_CHECKSUM_ERROR 2
+#define DHT_TIMEOUT 1
+#define DHT_CHECKSUM 2
 
-uint16_t lum_data = 0;
-uint16_t raw_lum_data = 0;
-float temp;
-float humid;
+
 
 void setup() 
 {
@@ -22,9 +19,10 @@ void setup()
 
 void loop() 
 {
-  raw_lum_data = analogRead(A0);
-  lum_data = 0.2*map(raw_lum_data,0 ,1024 ,0 ,5000 );
-  int error = readDHT11(&temp, &humid);
+  uint8_t temp;
+  uint8_t humid;
+  uint16_t lum_data = readLum();
+  uint8_t error = readTemp(&temp, &humid);
   if(error != DHT_SUCCESS)
   {
     Serial.print("Error using DHT11");
@@ -42,106 +40,90 @@ void loop()
   delay(1000);
 }
 
+uint16_t readLum( void )
+{
+  uint16_t raw_lum_data = analogRead(A0);
+  return (0.2*map(raw_lum_data,0 ,1024 ,0 ,5000 ));
+}
 
-uint8_t readDHT11(float* temp, float* humid) 
+uint8_t readTemp(uint8_t *temp, uint8_t *humid) 
 { 
   uint8_t data[5];
   data[0] = data[1] = data[2] = data[3] = data[4] = 0;
-  unsigned long start_time = 18;
+  unsigned long start_t = 18;
   unsigned long timeout = 1000;
-   /* Conversion du numéro de broche Arduino en ports / masque binaire "bas niveau" */
   uint8_t bit = digitalPinToBitMask(pin_humTemp_in);
   uint8_t port = digitalPinToPort(pin_humTemp_in);
   volatile uint8_t *ddr = portModeRegister(port);   // Registre MODE (INPUT / OUTPUT)
   volatile uint8_t *out = portOutputRegister(port); // Registre OUT (écriture)
   volatile uint8_t *in = portInputRegister(port);   // Registre IN (lecture)
-  
-  /* Conversion du temps de timeout en nombre de cycles processeur */
+  // calcul du nombre de cycles en fonction du temps de timeout
   unsigned long max_cycles = microsecondsToClockCycles(timeout);
-   /* Evite les problèmes de pull-up */
+
   *out |= bit;  // PULLUP
   *ddr &= ~bit; // INPUT
-  delay(100);   // Laisse le temps à la résistance de pullup de mettre la ligne de données à HIGH
- 
-  /* Réveil du capteur */
+  delay(100);   //
+  // Activation du capteur
   *ddr |= bit;  // OUTPUT
   *out &= ~bit; // LOW
-  delay(start_time); // Temps d'attente à LOW causant le réveil du capteur
-  // N.B. Il est impossible d'utilise delayMicroseconds() ici car un délai
-  // de plus de 16 millisecondes ne donne pas un timing assez précis.
-  
-  /* Portion de code critique - pas d'interruptions possibles */
+  delay(start_t); 
+  // Desactivation des intéruptions
   noInterrupts();
-  
-  /* Passage en écoute */
+  // Attente de la réponse du capteur
   *out |= bit;  // PULLUP
   delayMicroseconds(40);
   *ddr &= ~bit; // INPUT
  
-  /* Attente de la réponse du capteur */
   timeout = 0;
-  while(!(*in & bit)) { /* Attente d'un état LOW */
+  while(!(*in & bit)) { // Attente d'un etat bas
     if (++timeout == max_cycles) {
         interrupts();
-        return DHT_TIMEOUT_ERROR;
+        return DHT_TIMEOUT;
       }
   }
     
   timeout = 0;
-  while(*in & bit) { /* Attente d'un état HIGH */
+  while(*in & bit) { // Attente d'un etat haut
     if (++timeout == max_cycles) {
         interrupts();
-        return DHT_TIMEOUT_ERROR;
+        return DHT_TIMEOUT;
       }
   }
 
-  /* Lecture des données du capteur (40 bits) */
+  // Lecture de 40 bits de données
   for (byte i = 0; i < 40; ++i) {
  
-    /* Attente d'un état LOW */
     unsigned long cycles_low = 0;
     while(!(*in & bit)) {
       if (++cycles_low == max_cycles) {
         interrupts();
-        return DHT_TIMEOUT_ERROR;
+        return DHT_TIMEOUT;
       }
     }
-
-    /* Attente d'un état HIGH */
     unsigned long cycles_high = 0;
     while(*in & bit) {
       if (++cycles_high == max_cycles) {
         interrupts();
-        return DHT_TIMEOUT_ERROR;
+        return DHT_TIMEOUT;
       }
     }
-    
-    /* Si le temps haut est supérieur au temps bas c'est un "1", sinon c'est un "0" */
     data[i / 8] <<= 1;
     if (cycles_high > cycles_low) {
       data[i / 8] |= 1;
     }
   }
   
-  /* Fin de la portion de code critique */
+// réactivation des intéeuptions
   interrupts();
- 
-  /*
-   * Format des données :
-   * [1, 0] = humidité en %
-   * [3, 2] = température en degrés Celsius
-   * [4] = checksum (humidité + température)
-   */
    
-  /* Vérifie la checksum */
+// Vérification du CRC
   byte checksum = (data[0] + data[1] + data[2] + data[3]) & 0xff;
   if (data[4] != checksum)
-    return DHT_CHECKSUM_ERROR; /* Erreur de checksum */
+    return DHT_CHECKSUM;
   else
   {
-    temp = data[0];
-    humid = data[2];
-    Serial.println("Coucou Connard");
-    return DHT_SUCCESS; /* Pas d'erreur */
+    *temp = data[2];
+    *humid = data[0];
+    return DHT_SUCCESS;
   }
 }
